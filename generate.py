624fd30669aed450d29caf7440e70e08618fb268
Eyu-148 <ychen49@uvm.edu>
2022-01-29 16:32:41 -0500

    assignment3-manylinks

--- generate.py
+++ generate.py
@@ -1,5 +1,28 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+
+# initialize the positions
+x = 0
+y = 0
+z = 0.5
+for i in range(3):
+    # initializa size (1x1x1 box) and x-coordinate
+    x = 0
+    length = 1
+    width = 1
+    height = 1
+    for j in range(3):
+        # initialize size (1x1x1 box)
+        length = 1
+        width = 1
+        height = 1
+        for n in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[width, length, height])
+            z += 1
+            width = width*0.9
+            length = length*0.9
+            height = height*0.9
+        x += 1 
+    y += 1
 pyrosim.End()
